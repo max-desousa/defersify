5fc2f6cc70475844c23b40f3cb071af9a2b4da94
Max DeSousa <maxdesousa@protonmail.com>
2025-01-29 19:53:24 -0500

    Mini hal is crashing while out of the house...
    commiting to save SOME of what I was doing...
    but this change is garbo... mini hal may be dead :(

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module defersify
 
-go 1.18
+go 1.23.5
--- main.go
+++ main.go
@@ -3,11 +3,35 @@ package main
 import (
   "flag"
   "fmt"
+  "strings"
 )
 
+var Extensions []string
+var SupportedExtensions []string = []string{"c"}
+
 func main() {
+  /*****************************************************************************
+   * Handling runtime flags
+   ****************************************************************************/
   undo := flag.Bool("u", false, "Run \"un-defersify\" to revert files back to their original state.")
+  verbose := flag.Bool("v", false, "run with verbose output")
+  extensions := flag.String("ex", "c", "File extensions to subject to defersification")
   flag.Parse()
+
+  /* Convert user submitted extensions into a list we can use */
+  Extensions = strings.Split(*extensions, ",")
+
+  if (true == *verbose) { 
+    fmt.Println("Running defersify...") 
+    fmt.Printf("\n")
+    fmt.Println("You are running the program on files with extension:")
+    for _, val := range(Extensions) {
+      fmt.Printf("\t.%v\n", val)
+      //if SupportedExtensions.Contains(
+    }
+  }
+
+  fmt.Printf("\n")
   if (true == *undo) {
     undefersify()
   } else {
